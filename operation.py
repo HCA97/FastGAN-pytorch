98e49041461b66a1218c965b5f7acd55450a64e4
hca <s7hialtu@uni-bonn.de>
2021-05-20 15:24:07 +0200

    adjust the code for cube + dependecies added

--- operation.py
+++ operation.py
@@ -1,3 +1,5 @@
+import lmdb
+from io import BytesIO
 import os
 import numpy as np
 import torch
@@ -7,6 +9,7 @@ from PIL import Image
 from copy import deepcopy
 import shutil
 import json
+
 
 def InfiniteSampler(n):
     """Data sampler"""
@@ -23,6 +26,7 @@ def InfiniteSampler(n):
 
 class InfiniteSamplerWrapper(data.sampler.Sampler):
     """Data sampler wrapper"""
+
     def __init__(self, data_source):
         self.num_samples = len(data_source)
 
@@ -36,7 +40,7 @@ class InfiniteSamplerWrapper(data.sampler.Sampler):
 def copy_G_params(model):
     flatten = deepcopy(list(p.data for p in model.parameters()))
     return flatten
-    
+
 
 def load_params(model, new_param):
     for p, new_p in zip(model.parameters(), new_param):
@@ -44,27 +48,31 @@ def load_params(model, new_param):
 
 
 def get_dir(args):
-    task_name = 'train_results/' + args.name
-    saved_model_folder = os.path.join( task_name, 'models')
-    saved_image_folder = os.path.join( task_name, 'images')
-    
+    # task_name = "train_results/" + args.name
+    task_name = os.path.join('../tests/train_results', args.name)
+    # task_name = os.path.join(
+    #     "/scratch/s7hialtu/fast_gan/train_results", args.name)
+    saved_model_folder = os.path.join(task_name, 'models')
+    saved_image_folder = os.path.join(task_name, 'images')
+
     os.makedirs(saved_model_folder, exist_ok=True)
     os.makedirs(saved_image_folder, exist_ok=True)
 
     for f in os.listdir('./'):
         if '.py' in f:
             shutil.copy(f, task_name+'/'+f)
-    
-    with open( os.path.join(saved_model_folder, '../args.txt'), 'w') as f:
+
+    with open(os.path.join(saved_model_folder, '../args.txt'), 'w') as f:
         json.dump(args.__dict__, f, indent=2)
 
     return saved_model_folder, saved_image_folder
 
 
-class  ImageFolder(Dataset):
+class ImageFolder(Dataset):
     """docstring for ArtDataset"""
+
     def __init__(self, root, transform=None):
-        super( ImageFolder, self).__init__()
+        super(ImageFolder, self).__init__()
         self.root = root
 
         self.frame = self._parse_frame()
@@ -76,7 +84,7 @@ class  ImageFolder(Dataset):
         img_names.sort()
         for i in range(len(img_names)):
             image_path = os.path.join(self.root, img_names[i])
-            if image_path[-4:] == '.jpg' or image_path[-4:] == '.png' or image_path[-5:] == '.jpeg': 
+            if image_path[-4:] == '.jpg' or image_path[-4:] == '.png' or image_path[-5:] == '.jpeg':
                 frame.append(image_path)
         return frame
 
@@ -86,17 +94,11 @@ class  ImageFolder(Dataset):
     def __getitem__(self, idx):
         file = self.frame[idx]
         img = Image.open(file).convert('RGB')
-            
+
         if self.transform:
-            img = self.transform(img) 
+            img = self.transform(img)
 
         return img
-
-
-
-from io import BytesIO
-import lmdb
-from torch.utils.data import Dataset
 
 
 class MultiResolutionDataset(Dataset):
@@ -114,7 +116,8 @@ class MultiResolutionDataset(Dataset):
             raise IOError('Cannot open lmdb dataset', path)
 
         with self.env.begin(write=False) as txn:
-            self.length = int(txn.get('length'.encode('utf-8')).decode('utf-8'))
+            self.length = int(
+                txn.get('length'.encode('utf-8')).decode('utf-8'))
 
         self.resolution = resolution
         self.transform = transform
@@ -134,4 +137,3 @@ class MultiResolutionDataset(Dataset):
         img = self.transform(img)
 
         return img
-
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import lpips
 import torch
 from torch import nn
 import torch.optim as optim
@@ -13,8 +14,7 @@ from models import weights_init, Discriminator, Generator
 from operation import copy_G_params, load_params, get_dir
 from operation import ImageFolder, InfiniteSamplerWrapper
 from diffaug import DiffAugment
-policy = 'color,translation'
-import lpips
+policy = 'color'  # ,translation'
 percept = lpips.PerceptualLoss(model='net-lin', net='vgg', use_gpu=True)
 
 
@@ -23,31 +23,33 @@ percept = lpips.PerceptualLoss(model='net-lin', net='vgg', use_gpu=True)
 
 def crop_image_by_part(image, part):
     hw = image.shape[2]//2
-    if part==0:
-        return image[:,:,:hw,:hw]
-    if part==1:
-        return image[:,:,:hw,hw:]
-    if part==2:
-        return image[:,:,hw:,:hw]
-    if part==3:
-        return image[:,:,hw:,hw:]
+    if part == 0:
+        return image[:, :, :hw, :hw]
+    if part == 1:
+        return image[:, :, :hw, hw:]
+    if part == 2:
+        return image[:, :, hw:, :hw]
+    if part == 3:
+        return image[:, :, hw:, hw:]
+
 
 def train_d(net, data, label="real"):
     """Train function of discriminator"""
-    if label=="real":
+    if label == "real":
         pred, [rec_all, rec_small, rec_part], part = net(data, label)
-        err = F.relu(  torch.rand_like(pred) * 0.2 + 0.8 -  pred).mean() + \
-            percept( rec_all, F.interpolate(data, rec_all.shape[2]) ).sum() +\
-            percept( rec_small, F.interpolate(data, rec_small.shape[2]) ).sum() +\
-            percept( rec_part, F.interpolate(crop_image_by_part(data, part), rec_part.shape[2]) ).sum()
+        err = F.relu(torch.rand_like(pred) * 0.2 + 0.8 - pred).mean() + \
+            percept(rec_all, F.interpolate(data, rec_all.shape[2])).sum() +\
+            percept(rec_small, F.interpolate(data, rec_small.shape[2])).sum() +\
+            percept(rec_part, F.interpolate(
+                crop_image_by_part(data, part), rec_part.shape[2])).sum()
         err.backward()
         return pred.mean().item(), rec_all, rec_small, rec_part
     else:
         pred = net(data, label)
-        err = F.relu( torch.rand_like(pred) * 0.2 + 0.8 + pred).mean()
+        err = F.relu(torch.rand_like(pred) * 0.2 + 0.8 + pred).mean()
         err.backward()
         return pred.mean().item()
-        
+
 
 def train(args):
 
@@ -67,19 +69,19 @@ def train(args):
     current_iteration = 0
     save_interval = 100
     saved_model_folder, saved_image_folder = get_dir(args)
-    
+
     device = torch.device("cpu")
     if use_cuda:
         device = torch.device("cuda:0")
 
     transform_list = [
-            transforms.Resize((int(im_size),int(im_size))),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-        ]
+        transforms.Resize((int(im_size), int(im_size))),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ]
     trans = transforms.Compose(transform_list)
-    
+
     if 'lmdb' in data_root:
         from operation import MultiResolutionDataset
         dataset = MultiResolutionDataset(data_root, trans, 1024)
@@ -87,15 +89,14 @@ def train(args):
         dataset = ImageFolder(root=data_root, transform=trans)
 
     dataloader = iter(DataLoader(dataset, batch_size=batch_size, shuffle=False,
-                      sampler=InfiniteSamplerWrapper(dataset), num_workers=dataloader_workers, pin_memory=True))
+                                 sampler=InfiniteSamplerWrapper(dataset), num_workers=dataloader_workers, pin_memory=True))
     '''
     loader = MultiEpochsDataLoader(dataset, batch_size=batch_size, 
                                shuffle=True, num_workers=dataloader_workers, 
                                pin_memory=True)
     dataloader = CudaDataLoader(loader, 'cuda')
     '''
-    
-    
+
     #from model_s import Generator, Discriminator
     netG = Generator(ngf=ngf, nz=nz, im_size=im_size)
     netG.apply(weights_init)
@@ -116,7 +117,7 @@ def train(args):
 
     optimizerG = optim.Adam(netG.parameters(), lr=nlr, betas=(nbeta1, 0.999))
     optimizerD = optim.Adam(netD.parameters(), lr=nlr, betas=(nbeta1, 0.999))
-    
+
     if checkpoint != 'None':
         ckpt = torch.load(checkpoint)
         netG.load_state_dict(ckpt['g'])
@@ -136,16 +137,18 @@ def train(args):
         fake_images = netG(noise)
 
         real_image = DiffAugment(real_image, policy=policy)
-        fake_images = [DiffAugment(fake, policy=policy) for fake in fake_images]
-        
-        ## 2. train Discriminator
+        fake_images = [DiffAugment(fake, policy=policy)
+                       for fake in fake_images]
+
+        # 2. train Discriminator
         netD.zero_grad()
 
-        err_dr, rec_img_all, rec_img_small, rec_img_part = train_d(netD, real_image, label="real")
+        err_dr, rec_img_all, rec_img_small, rec_img_part = train_d(
+            netD, real_image, label="real")
         train_d(netD, [fi.detach() for fi in fake_images], label="fake")
         optimizerD.step()
-        
-        ## 3. train Generator
+
+        # 3. train Generator
         netG.zero_grad()
         pred_g = netD(fake_images, "fake")
         err_g = -pred_g.mean()
@@ -157,42 +160,53 @@ def train(args):
             avg_p.mul_(0.999).add_(0.001 * p.data)
 
         if iteration % 100 == 0:
-            print("GAN: loss d: %.5f    loss g: %.5f"%(err_dr, -err_g.item()))
+            print("GAN: loss d: %.5f    loss g: %.5f" %
+                  (err_dr, -err_g.item()))
 
         if iteration % (save_interval*10) == 0:
             backup_para = copy_G_params(netG)
             load_params(netG, avg_param_G)
             with torch.no_grad():
-                vutils.save_image(netG(fixed_noise)[0].add(1).mul(0.5), saved_image_folder+'/%d.jpg'%iteration, nrow=4)
-                vutils.save_image( torch.cat([
-                        F.interpolate(real_image, 128), 
-                        rec_img_all, rec_img_small,
-                        rec_img_part]).add(1).mul(0.5), saved_image_folder+'/rec_%d.jpg'%iteration )
+                vutils.save_image(netG(fixed_noise)[0].add(1).mul(
+                    0.5), saved_image_folder+'/%d.jpg' % iteration, nrow=4)
+                vutils.save_image(torch.cat([
+                    F.interpolate(real_image, 128),
+                    rec_img_all, rec_img_small,
+                    rec_img_part]).add(1).mul(0.5), saved_image_folder+'/rec_%d.jpg' % iteration)
             load_params(netG, backup_para)
 
         if iteration % (save_interval*50) == 0 or iteration == total_iterations:
             backup_para = copy_G_params(netG)
             load_params(netG, avg_param_G)
-            torch.save({'g':netG.state_dict(),'d':netD.state_dict()}, saved_model_folder+'/%d.pth'%iteration)
+            torch.save({'g': netG.state_dict(), 'd': netD.state_dict()},
+                       saved_model_folder+'/%d.pth' % iteration)
             load_params(netG, backup_para)
-            torch.save({'g':netG.state_dict(),
-                        'd':netD.state_dict(),
+            torch.save({'g': netG.state_dict(),
+                        'd': netD.state_dict(),
                         'g_ema': avg_param_G,
                         'opt_g': optimizerG.state_dict(),
-                        'opt_d': optimizerD.state_dict()}, saved_model_folder+'/all_%d.pth'%iteration)
+                        'opt_d': optimizerD.state_dict()}, saved_model_folder+'/all_%d.pth' % iteration)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='region gan')
 
-    parser.add_argument('--path', type=str, default='../lmdbs/art_landscape_1k', help='path of resource dataset, should be a folder that has one or many sub image folders inside')
-    parser.add_argument('--cuda', type=int, default=0, help='index of gpu to use')
-    parser.add_argument('--name', type=str, default='test1', help='experiment name')
-    parser.add_argument('--iter', type=int, default=50000, help='number of iterations')
-    parser.add_argument('--start_iter', type=int, default=0, help='the iteration to start training')
-    parser.add_argument('--batch_size', type=int, default=8, help='mini batch number of images')
-    parser.add_argument('--im_size', type=int, default=1024, help='image resolution')
-    parser.add_argument('--ckpt', type=str, default='None', help='checkpoint weight path if have one')
-
+    parser.add_argument('--path', type=str, default='../lmdbs/art_landscape_1k',
+                        help='path of resource dataset, should be a folder that has one or many sub image folders inside')
+    parser.add_argument('--cuda', type=int, default=0,
+                        help='index of gpu to use')
+    parser.add_argument('--name', type=str, default='test1',
+                        help='experiment name')
+    parser.add_argument('--iter', type=int, default=50000,
+                        help='number of iterations')
+    parser.add_argument('--start_iter', type=int, default=0,
+                        help='the iteration to start training')
+    parser.add_argument('--batch_size', type=int, default=8,
+                        help='mini batch number of images')
+    parser.add_argument('--im_size', type=int,
+                        default=1024, help='image resolution')
+    parser.add_argument('--ckpt', type=str, default='None',
+                        help='checkpoint weight path if have one')
 
     args = parser.parse_args()
     print(args)
